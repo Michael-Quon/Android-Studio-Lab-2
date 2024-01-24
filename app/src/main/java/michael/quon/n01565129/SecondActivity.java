66aace0cbf9589d3c45abd3a00cf411f4fdce6bb
Michael-Quon <michaelquon08@gmail.com>
2024-01-24 01:13:30 -0500

    Added a Back Arrow to the second screen to go back to the main screen.

--- app/src/main/java/michael/quon/n01565129/SecondActivity.java
+++ app/src/main/java/michael/quon/n01565129/SecondActivity.java
@@ -2,9 +2,14 @@ package michael.quon.n01565129;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.google.android.material.snackbar.Snackbar;
 
 public class SecondActivity extends AppCompatActivity {
@@ -14,13 +19,19 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
         TextView textView = findViewById(R.id.textView);
         String value = getIntent().getStringExtra("name");
         textView.setText(value);
 
         // Snackbar action to go back to the FirstActivity
         View parentLayout = findViewById(android.R.id.content);
-        Snackbar.make(parentLayout, "Display Main Screen", Snackbar.LENGTH_LONG)
+        Snackbar.make(parentLayout, "Back to Main Screen", Snackbar.LENGTH_LONG)
                 .setAction("BACK", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -32,4 +43,14 @@ public class SecondActivity extends AppCompatActivity {
                 .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
                 .show();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
