d174db25f14d69e97fd4c04b09df1fc43709096f
Michael-Quon <michaelquon08@gmail.com>
2024-01-23 23:02:30 -0500

    Added a snackbar to the second screen to intercept user press on Soft back key, when pressed goes back to main screen

--- app/src/main/java/michael/quon/n01565129/SecondActivity.java
+++ app/src/main/java/michael/quon/n01565129/SecondActivity.java
@@ -1,8 +1,11 @@
 package michael.quon.n01565129;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView textView = findViewById(R.id.textView);
+        String value = getIntent().getStringExtra("name");
+        textView.setText(value);
+
+        // Snackbar action to go back to the FirstActivity
+        View parentLayout = findViewById(android.R.id.content);
+        Snackbar.make(parentLayout, "Display Main Screen", Snackbar.LENGTH_LONG)
+                .setAction("BACK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(SecondActivity.this, FirstActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setDuration(20000)
+                .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
+                .show();
     }
 }
